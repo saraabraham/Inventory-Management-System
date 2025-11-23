2602ce9ec91758c7b300977d65538531eb9d7441
Sara Abraham <saraabraham765@gmail.com>
2025-11-23 17:01:38 +0100

    Hardcode production API URL

--- frontend/inventory-client/src/services/api.ts
+++ frontend/inventory-client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5174/api';
+const API_BASE_URL = 'https://inventory-api-webapp-8b52885b.azurewebsites.net/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
